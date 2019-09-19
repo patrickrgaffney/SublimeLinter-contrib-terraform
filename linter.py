9c33ddb441e89734d6bcadf315ef52aacbe2b72f
Pat Gaffney <pat@hypepat.com>
2019-09-18 23:04:00 -0500

    Override the run() method to parse the JSON directly

--- linter.py
+++ linter.py
@@ -1,6 +1,11 @@
 import re
 from SublimeLinter.lint import Linter, ERROR
+from SublimeLinter.lint.linter import LintMatch
 from os.path import basename
+import json
+import logging
+
+logger = logging.getLogger('SublimeLinter.plugin.terraform')
 
 # Compile the multi-line regular expression
 # to pull the relvent bits from the JSON output.
@@ -41,43 +46,88 @@ class Terraform(Linter):
     # Turn off stdin. The validate command requires a directory.
     template_suffix = '-'
 
-    def split_match(self, match):
+    def find_errors(self, output):
     	"""
-    	Override to fix the "message" output and to determine if we
-			should lint this file.
+    	Override the find_errors() output so that we can parse the JSON
+    	output directly instead of using a multiline regex to walk
+    	through it.
     	"""
-    	match, line, col, error, warning, message, near = (
-    		super().split_match(match)
-    	)
+    	print(output)
+    	try:
+    		data = json.loads(output)
+    	except Exception as e:
+    		logger.warning(e)
+    		self.notify_failure()
 
-    	matched_file = match.groupdict()['filename']
-    	linted_file = basename(self.filename)
+    	# If there are no errors, return to stop iteration,
+    	# then yield to turn this function into a generator.
+    	if data["valid"]:
+    		return
+    		yield
 
-    	# If the current file being linted is not the file wherein
-    	# we matched on an error, we override the return to avoid
-    	# showing "ghost" errors in the wrong file.
-    	if matched_file != linted_file:
-    		return None, None, None, None, None, '', None
+    	# Iterate through the errors, yielding LintMatchs.
+    	for issue in data['diagnostics']:
+    		message = '{summary}: {detail}'.format(
+    			summary=issue['summary'],
+    			detail=issue['detail']
+    		)
+    		severity = issue["severity"]
+    		error = severity if severity == "error" else ""
+    		warning = severity if severity == "warning" else ""
+    		line = issue["range"]["start"]["line"]
+    		col = issue["range"]["start"]["column"]
+    		filename = issue["range"]["filename"]
 
-    	new_msg = clean_up_message(message)
+    		lm = LintMatch(
+    			match={message, error, warning, line, col, filename},
+    			line=int(line),
+    			col=int(col),
+    			error=error,
+    			warning=warning,
+    			message=message
+    		)
+    		print(lm)
+    		yield lm
 
-    	return match, line, col, error, warning, new_msg, near
+   #  def split_match(self, match):
+   #  	"""
+   #  	Override to fix the "message" output and to determine if we
+			# should lint this file.
+   #  	"""
+   #  	match, line, col, error, warning, message, near = (
+   #  		super().split_match(match)
+   #  	)
+
+   #  	print({match, line, col, error, warning, message, near})
+
+   #  	# matched_file = match.groupdict()['filename']
+   #  	# linted_file = basename(self.filename)
+
+   #  	# # If the current file being linted is not the file wherein
+   #  	# # we matched on an error, we override the return to avoid
+   #  	# # showing "ghost" errors in the wrong file.
+   #  	# if matched_file != linted_file:
+   #  	# 	return None, None, None, None, None, '', None
+
+   #  	# new_msg = clean_up_message(message)
+
+   #  	return match, line, col, error, warning, new_msg, near
 
 # Terraform validate returns a summary and a detail. We include both
 # in the named capture group "message". These are matched over two
 # separate JSON keys, so the string sent to split_match() contains
 # a lot of extra junk -- namely the "detail" key and some whitespace.
-def clean_up_message(msg):
-	# Split the message on the end of the "message" key value.
-	split_msg = msg.split('",')
+# def clean_up_message(msg):
+# 	# Split the message on the end of the "message" key value.
+# 	split_msg = msg.split('",')
 
-	# The first match is the value from the "summary" key.
-	summary = split_msg[0]
+# 	# The first match is the value from the "summary" key.
+# 	summary = split_msg[0]
 
-	# Remove the whitespace and "detail" key from the message.
-	raw_detail = re.sub(r'\s*\"detail\":\s*\"', '', split_msg[1])
+# 	# Remove the whitespace and "detail" key from the message.
+# 	raw_detail = re.sub(r'\s*\"detail\":\s*\"', '', split_msg[1])
 
-	# Un-escape all JSON-escape double-quotes.
-	detail = raw_detail.replace('\\"', '"')
+# 	# Un-escape all JSON-escape double-quotes.
+# 	detail = raw_detail.replace('\\"', '"')
 
-	return '{summary}: {detail}'.format(summary=summary, detail=detail)
+# 	return '{summary}: {detail}'.format(summary=summary, detail=detail)
